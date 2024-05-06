99de001f8c77961ba04e78da716df3063bd067bc
KatairKrofind <bayandueva00@gmail.com>
2024-05-06 22:46:43 +0800

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,16 @@
+my_list = ['apple', 'orange', 'melon', 'peach', 'mango']
+print('List: ',my_list)
+print('First element: ', my_list[0])
+print('Last element: ', my_list[-1])
+print('Sublist: ', my_list[2:5])
+my_list[2] = 'pear'
+print('Modified list:', my_list)
+
+print('')
+
+my_dict = {'apple': 'яблоко', 'orange': 'апельсин', 'melon': 'дыня', 'peach': 'персик', 'mango': 'манго'}
+print('Dictionary: ', my_dict)
+print('Translation: ', my_dict['melon'])
+my_dict['orange'] = 'оранжевый'
+my_dict ['kiwi'] = 'киви'
+print('Modified list: ', my_dict)
